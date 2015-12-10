c8c84748dfe8d8c0c9116f18f8c73274cf918316
Sonia <sonia.pepe@fdvsolutions.com>
2015-12-10 16:32:48 -0300

    Adding routes to the app

--- myToDoList/www/js/controllers.js
+++ myToDoList/www/js/controllers.js
@@ -1,14 +1,11 @@
 app.controller('toDoCtrl', function($scope) {
-
     $scope.todos = [];
-
     $scope.done = function(todo) {
     	var indexOf = $scope.todos.indexOf(todo);
     	if (indexOf !== -1) {
     		$scope.todos.splice(indexOf, 1);
     	};
     };
-
     $scope.add = function(e) {
     	if (e.which && e.which === 13) {
     		$scope.todos.push($scope.newTodo);
@@ -16,3 +13,34 @@ app.controller('toDoCtrl', function($scope) {
     	};
     };
 });
+
+
+app.controller('notesCtrl', function($scope) {
+	$scope.notes = [];
+	$scope.erase = function(note) {
+		var indexOf = $scope.notes.indexOf(note);
+		if (indexOf !== -1) {
+			$scope.todos.splice(indexOf, 1);
+		};
+	};
+
+	$scope.add = function(e) {
+		if (e.which && e.which === 13) {
+			$scope.notes.push($scope.newNote);
+			$scope.newNote = " ";
+		};
+	}
+});
+
+
+app.config(function($routeProvider) {
+	$routeProvider
+	.when('/todos', {
+		templateUrl : 'tmp/todos.html',
+        controller  : 'toDoCtrl'
+	})
+	.when('/notes', {
+		templateUrl : 'tmp/notes.html',
+		controller  : 'notesCtrl'
+	})
+})
